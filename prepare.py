7132379a1ebde6a088340b748630cefe5f3fcaa9
Yifei <ycheng517@gmail.com>
2016-04-24 19:06:37 -0400

    bug

--- prepare.py
+++ prepare.py
@@ -2,7 +2,7 @@ from prepare_db import player_avg_pts
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client.nba
+db = client.nba_py
 
 ap = player_avg_pts.PlayerAvgPts(db.model)
 ap.calc_avg_pts()
