e51d7a6520fe6c585c8e6927d90c922dd761f665
Yifei <yifei@kindred.ai>
2016-08-29 00:29:53 -0400

    update team_id in game logs

--- load_dbs/game_logs.py
+++ load_dbs/game_logs.py
@@ -1,4 +1,5 @@
 from nba_py import league
+from nba_py.constants import TEAMS
 from pymongo import MongoClient
 
 seasons = ['2013-14', '2014-15', '2015-16']
@@ -6,11 +7,20 @@ seasons = ['2013-14', '2014-15', '2015-16']
 client = MongoClient()
 db = client.nba_stats
 
-for season in seasons:
-    ap = league.GameLog(season=season)
-    logs = ap.overall()
-    print(len(logs))
-    db.game_stats.insert_many(logs)
+#===============================================================================
+# for season in seasons:
+#     ap = league.GameLog(season=season)
+#     logs = ap.overall()
+#     print(len(logs))
+#     db.game_stats.insert_many(logs)
+#===============================================================================
+
+game_logs = db.game_stats.find()
+for log in game_logs:
+    team_id = TEAMS[log['TEAM_ABBREVIATION']]['id']
+    db.game_stats.update_one({"_id": log['_id']},
+                             {'$set':
+                                {"TEAM_ID": team_id}})
     #===========================================================================
     # players_list = logs.to_dict('records')
     # for d in players_list: 
