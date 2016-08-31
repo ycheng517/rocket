e8fc786c331e0fdbc665f08c402033b5824f92b5
Yifei Cheng <ycheng517@gmail.com>
2016-08-31 03:39:07 +0000

    finish game summary

--- load_dbs/game_summary.py
+++ load_dbs/game_summary.py
@@ -7,11 +7,12 @@ client = MongoClient()
 db = client.nba_stats
 
 game_ids = db.game_stats.distinct("GAME_ID")
-
+print(len(game_ids))
 count = 0
 for game_id in game_ids:
     count += 1
     print count
     game_summary = game.Boxscore(str(game_id)).game_summary()
-    print game_summary
-    db.game_summary.insert_many(game_summary)
+    if len(game_summary) > 1:
+        print game_summary
+    db.game_summary.insert_one(game_summary[0])
