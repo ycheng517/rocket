c32581169ae5d26ee08fc22de7a03e5cd57f28a5
Yifei <ycheng517@gmail.com>
2016-12-13 22:51:53 -0500

    player shooting splits

---
+++ retrieve_data_from_web/player_shooting_splits.py
@@ -0,0 +1,32 @@
+import pandas
+from nba_py import player as nba_py_player
+from utilities import season_to_season_id
+
+
+class PlayerShootingSplits(object):
+    def __init__(self, db, seasons):
+        self.db = db
+        self.seasons = seasons
+        self.get_player_shooting_splits()
+
+    def get_player_shooting_splits(self):
+        for season in self.seasons:
+            player_list = nba_py_player.PlayerList(season=season).info()
+            if isinstance(player_list, pandas.DataFrame):
+                for index, row in player_list.iterrows():
+                    if self.db.player_shooting_splits.count({"SEASON_ID": season_to_season_id(season),
+                                                             "PLAYER_ID": str(row.PERSON_ID)}) > 0:
+                        print("shooting splits of {} {} already exists, skipping".format(season, row.DISPLAY_FIRST_LAST))
+                        continue
+                    print("retrieving player shooting splits for {} {}".format(season, row.DISPLAY_FIRST_LAST))
+                    shooting_splits = nba_py_player.PlayerShootingSplits(player_id=row.PERSON_ID, season=season).shot_5ft() 
+                    if isinstance(shooting_splits, pandas.DataFrame):
+                        shooting_splits = shooting_splits.to_dict("records")
+                    if shooting_splits:
+                        for shooting_range in shooting_splits:
+                            shooting_range['PLAYER_ID'] = str(row.PERSON_ID)
+                            shooting_range['SEASON_ID'] = season_to_season_id(season)
+                            shooting_range['PLAYER_NAME'] = row.DISPLAY_FIRST_LAST
+                        self.db.player_shooting_splits.insert_many(shooting_splits)
+                    else:
+                        print("shooting splits of {} {} not found".format(season, row.DISPLAY_FIRST_LAST))
--- setup_db.py
+++ setup_db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 import sys
-from retrieve_data_from_web import player_game_logs, team_game_logs
+from retrieve_data_from_web import player_game_logs, team_game_logs, player_shooting_splits
 
 assert str(sys.argv[1]) is not None
 client = MongoClient(str(sys.argv[1]))
@@ -10,6 +10,8 @@ db = client.nba_stats
 seasons = ['2007-08', '2008-09', '2009-10', '2010-11', '2011-12', '2012-13', '2013-14', '2014-15', '2015-16']
 # convesion: team ID is string, game ID is string, use season_id instead of season when possible
 
-player_game_logs.PlayerGameLogs(db, seasons, use_team_id=True)
+# player_game_logs.PlayerGameLogs(db, seasons, use_team_id=True)
 
-team_game_logs.TeamGameLogs(db, seasons)
+# team_game_logs.TeamGameLogs(db, seasons)
+
+player_shooting_splits.PlayerShootingSplits(db, seasons)
